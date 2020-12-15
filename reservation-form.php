b409965a33c55870abed499d49cde8482e9cc195
Laprevote Emma <emma.laprevote@laplateforme.io>
2020-12-15 16:33:16 +0100

    1er commit php page reservation

---
+++ reservation-form.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+            <form class="inscription" action="reservation-form.php" method="POST">
+                
+                <legend>RESERVATION</legend>
+                <br>
+                <div class="inputDiv">
+                    <label class="ins" for="login">Login<span>*</span> :</label>
+                    <input class="place" type="text" name="login" required placeholder="Nom d'utilisateur">
+                </div>
+                <br>
+                <div class="inputDiv">
+                    <label class="ins" for="passeword">Password<span>*</span> :</label>
+                    <input class="place" type="password" name="password" required placeholder="Password">
+                </div>
+                <br>
+                <div class="inputDiv">
+                    <label class="ins" for="confirm_passeword">Confirm password<span>*</span> :</label>
+                    <input class="place" type="password" name="confirm_password" required placeholder="Password">
+                </div>
+                <br>
+                <input id="button" type="submit" name="envoyer" value="Envoyer" />
+            </form>
+            <?php 
+            require_once('libraries/autoload.php');
+            session_start();
+            $signUp = $_SESSION['user'];
+
+                var_dump($_SESSION['user']);
+
+            ?>
+</body>
+</html>
